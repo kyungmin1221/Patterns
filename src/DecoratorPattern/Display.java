c385df118a4851ac2b416fad3ca0439bb5792966
kyungmin1221 <fox9872@naver.com>
2022-11-07 11:36:02 +0900

    pattern

---
+++ src/DecoratorPattern/Display.java
@@ -0,0 +1,5 @@
+package DecoratorPattern;
+
+public abstract class Display {
+    public abstract void draw();
+}
---
+++ src/DecoratorPattern/RoadDisplay.java
@@ -0,0 +1,7 @@
+package DecoratorPattern;
+
+public class RoadDisplay extends Display {
+    public void draw() {
+        System.out.println("기본 도로 표시");
+    }
+}
